0777e04efe6d089a3ff0ebe9f52a6867c87da034
Nidocq <phillip.linnemann.lundin@gmail.com>
2022-12-06 07:16:51 +0100

    Refactor day 6

--- day6/solution.swift
+++ day6/solution.swift
@@ -1,30 +1,28 @@
 import Foundation
 let File_Name_URL : URL = URL(fileURLWithPath: "./input.txt")
 let raw_input : String = try String(contentsOf: File_Name_URL, encoding: .utf8)
-
-let exerciseOne =
+let start_of_marker = 4
+let start_of_message = 14
+let unique_sequence =
     raw_input
         .reduce("") { x, y in
-            var tmp = x
-            // print(tmp)
-            if tmp.count > 14 {
-                // print("removing")
-                tmp.removeFirst()
-                // print("tmp after remove : \(tmp)")
+            var marker = x
+            if marker.count > start_of_marker {
+                marker.removeFirst()
             }
-            let length = tmp.count
-            if tmp.count == 14 {
+            let length = marker.count
+            if marker.count == start_of_marker {
                 // All characters are unique
-                if Set(tmp).count ==  length {
-                    // let kk = raw_input.components(separatedBy: tmp)
-                    return tmp
+                if Set(marker).count ==  length {
+                    return marker
                 }
             }
-            return tmp + String(y)
+            return marker + String(y)
         }
+        
+let upper_range = raw_input.range(of: unique_sequence)!.upperBound
 
+let sequence_index: Int =
+    raw_input[raw_input.startIndex..<upper_range].count
 
-
-// let exerciseTwo =
-    // raw_input
-print(raw_input[raw_input.startIndex..<raw_input.range(of: exerciseOne)!.upperBound].count)
+print(sequence_index)
